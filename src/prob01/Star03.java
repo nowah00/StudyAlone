6eb3700b28e75de11e185685c50c18df95dddff6
nowah00 <hawon2k@gmail.com>
2025-08-19 16:09:20 +0900

    first commit

---
+++ src/prob01/Star03.java
@@ -0,0 +1,23 @@
+package prob01;
+
+import java.util.Scanner;
+
+public class Star01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int a = n;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = a-1; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            a--;
+        }
+    }
+}
